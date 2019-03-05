2f928a3ccdfdc3700d9ed049d549a56d21db1ea0
RAJESH SHARMA <raj.sharma21@outlook.com>
2019-03-05 20:17:56 +1300

    update minesweeper

--- minesweeper.js
+++ minesweeper.js
@@ -45,7 +45,7 @@ var board = {
     {
       row: 1,
       col:2,
-      isMine: true,
+      isMine: false,
       isMarked: false,
       hidden: true
     },
@@ -92,23 +92,36 @@ var board = {
 
  // Don't remove this function call: it makes the game work!
 function startGame () {
+  document.addEventListener("click", checkForWin)
+  document.addEventListener("contextmenu", checkForWin)
   for (var i =0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines= countSurroundingMines(board.cells[i]);
   }
      lib.initBoard()
 }
+     
+
 
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
+  for (var i = 0; i < board.cells.length; i++) {
+    if (board.cells[i].isMine === true && board.cells[i].isMarked === true) {
+      return; 
+    } else if (board.cells[i].hidden === true && board.cells[i].isMine ===false) {
+      return;
+    } 
+    }
+     lib.displayMessage('You win!')
+
+    }
+  
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
   //   lib.displayMessage('You win!')
-}
-
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
 // cells yourself! Just use `lib.getSurroundingCells`: 
@@ -128,4 +141,24 @@ function countSurroundingMines (cell) {
   console.log(count);
   return count;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
